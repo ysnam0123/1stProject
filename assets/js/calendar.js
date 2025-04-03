156052b3218d4e1f8637be5e7e77ba5fa93a91e0
LEE JUNHO <sjsk3358@naver.com>
2025-04-03 12:31:56 +0900

    bind함수 추가

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -3,6 +3,7 @@ let currentYear = today.getFullYear();
 let currentMonth = today.getMonth();
 const leftBtn = document.getElementById('leftBtn');
 const rightBtn = document.getElementById('rightBtn');
+const cancelBtn = document.querySelector('.cancelBtn');
 let setMonth = '';
 let dateInfo = 0;
 
@@ -14,6 +15,7 @@ leftBtn.addEventListener('click',(e)=>{
     currentYear = currentYear - 1;
   }
   createCalendar(currentYear,currentMonth);
+  bind();
 });
 
 rightBtn.addEventListener('click',(e)=>{
@@ -24,9 +26,11 @@ rightBtn.addEventListener('click',(e)=>{
     currentYear = currentYear + 1;
   }
   createCalendar(currentYear,currentMonth);
+  bind();
 });
 
 createCalendar(currentYear,currentMonth);
+bind();
 
 function createCalendar(year, month){
   const calendar = document.getElementById('calendarWrapper');
@@ -95,15 +99,19 @@ function createCalendar(year, month){
     if(!JSON.parse(localStorage.getItem(dateInfo))) return;
   
     const datas = JSON.parse(localStorage.getItem(dateInfo)) || [];
-    let { color, title = "무제"} = datas[0] || [];
-    if(color === '색상') color = "#3389ff";
-
-    const li = document.createElement("li");
-    li.classList.add('planListItem');
-    li.textContent = title;
-    li.style.backgroundColor = color;
-
-    parenstEl.appendChild(li);
+    for(let i = 0; i < datas.length; i++){
+      let { color, title } = datas[i] || [];
+      if(color === '색상') color = "#3389ff";
+      if(title === '') title = "무제";
+  
+      console.log(title);
+      const li = document.createElement("li");
+      li.classList.add('planListItem');
+      li.textContent = title;
+      li.style.backgroundColor = color;
+  
+      parenstEl.appendChild(li);
+    }
   }
 
   for(let i = lastDay; i < weekDays.length-1; i++){
@@ -115,91 +123,95 @@ function createCalendar(year, month){
   calendar.appendChild(daysContainer);
 }
 
-const selectItems = document.querySelectorAll('.selectItem');
-const addPlanBtns = document.querySelectorAll('.selectItem .addPlanBtn');
-const saveBtn = document.querySelector('.saveBtn');
-const cancelBtn = document.querySelector('.cancelBtn');
 
-selectItems.forEach(item=>{
-  item.addEventListener('mouseenter',()=>{
-    item.querySelector('.addPlanBtn').classList.remove('hide');
-  });
-});
-
-selectItems.forEach(item=>{
-  item.addEventListener('mouseleave',()=>{
-    item.querySelector('.addPlanBtn').classList.add('hide');
-  });
-});
-
-addPlanBtns.forEach(btn=>{
-  btn.addEventListener('click',(e)=>{
-    e.preventDefault();
-    const parentEl = e.target.parentElement.parentElement;
-    dateInfo = parentEl.dataset.dateInfo;
-    document.querySelector('.shadow').classList.remove('hide');
-    document.querySelector('.modal').classList.remove('hide');
-  });
-});
-
-saveBtn.addEventListener('click',(e)=>{
-  e.preventDefault();
-  const title = document.getElementById("title");
-  const startday = document.getElementById("startday");
-  const endday = document.getElementById("endday");
-  const color = document.getElementById("colorInput");
-
-  const titleValue = title.value;
-  const startdayValue = startday.value.split('-').join('');
-  const enddayValue = endday.value.split('-').join('');
-  const colorValue = color.value; 
-
-  // inputValidation(title, startday, endday, color);
-
-  setStorage(dateInfo,titleValue,startdayValue,enddayValue,colorValue);
+function bind(){
+  // const cancelBtn = document.querySelector('.cancelBtn');
+  const selectItems = document.querySelectorAll('.selectItem');
+  const addPlanBtns = document.querySelectorAll('.selectItem .addPlanBtn');
+  const saveBtn = document.querySelector('.saveBtn');
   
-  clearValue();
-
-  document.querySelector('.shadow').classList.add('hide');
-  document.querySelector('.modal').classList.add('hide');
-
-  createCalendar(currentYear,currentMonth);
-});
-
-// function inputValidation(title, startday, endday, color){
-//   if(title.value === '' || color.value === '' || startday.value === '' || endday.value === ''){
-
-//   }
-// }
-
-function clearValue(){
-  document.getElementById("title").value = '';
-  document.getElementById("startday").value = '';
-  document.getElementById("endday").value = '';
-  document.getElementById("colorInput").selectedIndex = 0;
-}
-
-function setStorage(dataInfo,title,startday,endday,color){
-  const obj = {
-    'title' : title,
-    'startday' : startday,
-    'endday' : endday,
-    'color' : color,
+  selectItems.forEach(item=>{
+    item.addEventListener('mouseenter',()=>{
+      item.querySelector('.addPlanBtn').classList.remove('hide');
+    });
+  });
+  
+  selectItems.forEach(item=>{
+    item.addEventListener('mouseleave',()=>{
+      item.querySelector('.addPlanBtn').classList.add('hide');
+    });
+  });
+  
+  addPlanBtns.forEach(btn=>{
+    btn.addEventListener('click',(e)=>{
+      e.preventDefault();
+      const parentEl = e.target.parentElement.parentElement;
+      dateInfo = parentEl.dataset.dateInfo;
+      document.querySelector('.shadow').classList.remove('hide');
+      document.querySelector('.modal').classList.remove('hide');
+    });
+  });
+  
+  saveBtn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    const title = document.getElementById("title");
+    const startday = document.getElementById("startday");
+    const endday = document.getElementById("endday");
+    const color = document.getElementById("colorInput");
+  
+    const titleValue = title.value;
+    const startdayValue = startday.value.split('-').join('');
+    const enddayValue = endday.value.split('-').join('');
+    const colorValue = color.value; 
+  
+    // inputValidation(title, startday, endday, color);
+  
+    setStorage(dateInfo,titleValue,startdayValue,enddayValue,colorValue);
+    
+    clearValue();
+  
+    document.querySelector('.shadow').classList.add('hide');
+    document.querySelector('.modal').classList.add('hide');
+  
+    createCalendar(currentYear,currentMonth);
+    bind();
+  });
+  
+  // function inputValidation(title, startday, endday, color){
+  //   if(title.value === '' || color.value === '' || startday.value === '' || endday.value === ''){
+  
+  //   }
+  // }
+  
+  function clearValue(){
+    document.getElementById("title").value = '';
+    document.getElementById("startday").value = '';
+    document.getElementById("endday").value = '';
+    document.getElementById("colorInput").selectedIndex = 0;
   }
-  const temp = JSON.parse(localStorage.getItem(dataInfo)) || [];
-  temp.push({...obj});
-  //시작일~마감일까지 정보 로컬 스토리지에 추가
-  for(let i = +temp[temp.length-1].startday;i <= +temp[temp.length-1].endday;i++){
-    localStorage.setItem(`${i}`,JSON.stringify(temp));
-    console.log(i,temp);
+  
+  function setStorage(dataInfo,title,startday,endday,color){
+    const obj = {
+      'title' : title,
+      'startday' : startday,
+      'endday' : endday,
+      'color' : color,
+    }
+    const temp = JSON.parse(localStorage.getItem(dataInfo)) || [];
+    temp.push({...obj});
+    //시작일~마감일까지 정보 로컬 스토리지에 추가
+    for(let i = +temp[temp.length-1].startday;i <= +temp[temp.length-1].endday;i++){
+      localStorage.setItem(`${i}`,JSON.stringify(temp));
+      console.log(i,temp);
+    }
   }
+  
+  cancelBtn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    clearValue();
+    document.querySelector('.shadow').classList.add('hide');
+    document.querySelector('.modal').classList.add('hide');
+  });
 }
-
-cancelBtn.addEventListener('click',(e)=>{
-  e.preventDefault();
-  clearValue();
-  document.querySelector('.shadow').classList.add('hide');
-  document.querySelector('.modal').classList.add('hide');
-});
 
 // 인풋 값을 로컬 스토리지에 저장 -> calendar 함수 실행 -> daydiv.dataset으로 로컬 스토리지 키값에 접근-> []안의 객체에 접근 title color값 구조분해 배열의 길이만큼 daydiv안에 div 넣고 스타일링
